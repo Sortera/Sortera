f74a2f7f4e0df55286f0cb4fafe29d4232acfc75
Luke Van Bergen <lukepvb@gmail.com>
2020-02-19 10:35:47 -0800

    postgresDB initial setup

--- server/db/postgresDB.js
+++ server/db/postgresDB.js
@@ -1,13 +1,9 @@
 const { Pool } = require('pg');
-
-const myURI = 'postgres://pezehgtp:WsGMfyx2ne0D4RCoSWzEvs04pS3zxdHq@rajje.db.elephantsql.com:5432/pezehgtp';
-
-const URI = process.env.PG_URI || myURI;
-
+const pgURI =
+  'postgres://pezehgtp:WsGMfyx2ne0D4RCoSWzEvs04pS3zxdHq@rajje.db.elephantsql.com:5432/pezehgtp';
 
 const pool = new Pool({
-  connectionString = URI
-
+  connectionString: pgURI
 });
 
 module.exports = {
