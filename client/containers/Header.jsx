32d186f645ebd856e329491d5e0842eef8cfd8ba
Yunho Cho <ycho0502@gmail.com>
2020-02-19 15:17:12 -0800

    Organized and created containers and components, created upload button

---
+++ client/containers/Header.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render () {
+
+        return (
+            <div className="header">
+                Header
+            </div>
+        )
+        
+    }
+}
