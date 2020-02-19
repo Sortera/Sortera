6fa5a14b47cf4b40a18b3e8363a2d04d7b046959
Evan <egrobar@gmail.com>
2020-02-19 15:57:35 -0800

    better test images

--- client/App.jsx
+++ client/App.jsx
@@ -12,7 +12,7 @@ export default class App extends Component {
             //let "image"s = <tabbed"Image"s "image"s={this.state."image"List}></tabbed"Image"s>
         //}
 
-        let images = <TabbedImages images={[<img src='./assets/pineapple.png' className={"image"}></img>,<img src='./assets/trees.jpg' className={"image"}></img>,<img src='./assets/seth-rogen-premiere-this-is-the-end-04-copy.png' className={"image"}></img>]}></TabbedImages>
+        let images = <TabbedImages images={[<img src='./assets/pineapple.png' className={"image"}></img>,<img src='./assets/sunset.jpg' className={"image"}></img>,<img src='./assets/apple.jpg' className={"image"}></img>]}></TabbedImages>
 
         return (
             <div>
