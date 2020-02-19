8a22f74582b7e43901968fa8eff1d94d67863363
Luke Van Bergen <lukepvb@gmail.com>
2020-02-19 15:43:55 -0800

    edits to userController.createUser, fixed math.random error in authController when setting cookie

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -23,10 +23,10 @@ userController.createUser = (req, res, next) => {
   const { username, password, email } = req.body;
   console.log('req.body: ', req.body);
   const text = `
-    INSERT INTO users (username, password, email, TIMESTAMP)
+    INSERT INTO users (username, password, email, downloads)
     values($1, $2, $3, $4)
   `;
-  const values = [username, password, email, Date.now()];
+  const values = [username, password, email, 0];
   db.query(text, values)
     .then(response => console.log(response))
     .catch(err => console.log(err));
--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
-const bodyParser = require('body-parser');
+
 const cookieParser = require('cookie-parser');
 const userController = require('./controllers/userController');
 const authController = require('./controllers/authController');
 
-app.use(bodyParser.urlencoded( {extended: true} ));
+app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('assets'));
 
@@ -16,26 +16,35 @@ app.get('/', authController.createGeneralCookie, (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'));
 });
 
-app.post('/images', (req, res) => {
+app.post('/images', (req, res) => {});
 
-});
-
-app.get('/imageTags', (req, res) => {
-
-});
+app.get('/imageTags', (req, res) => {});
 
 // use this route to test or debug individual middleware one by one
 app.post('/test', userController.createUser, (req, res) => {
   res.redirect('/');
 });
 
-app.post('/login', userController.verifyUser, authController.createUserCookie, authController.startSession, (req, res) => {
-  res.redirect('/');
-});
+app.post(
+  '/login',
+  userController.verifyUser,
+  authController.createUserCookie,
+  authController.startSession,
+  (req, res) => {
+    res.redirect('/');
+  }
+);
 
-app.post('/signup', userController.checkUnique, userController.createUser, authController.createUserCookie, authController.startSession, (req, res) => {
-  res.redirect('/');
-});
+app.post(
+  '/signup',
+  userController.checkUnique,
+  userController.createUser,
+  authController.createUserCookie,
+  authController.startSession,
+  (req, res) => {
+    res.redirect('/');
+  }
+);
 
 app.use('*', (req, res) => {
   res.status(404).send('Page Not Found');
