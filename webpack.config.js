d7367683ff5c372140d710d160dd643f74444a66
Yunho Cho <ycho0502@gmail.com>
2020-02-19 16:02:26 -0800

    Fixed css-loader issue by reconfiguring webpack.config.js. Also, moved few npm dependencies to Dev Dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
 module.exports = {
@@ -27,8 +28,16 @@ module.exports = {
 
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
+        use: [
+            MiniCssExtractPlugin.loader,
+            { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
+            { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
+    },
+    {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+    },
       
       {
         test: /\.(png|jpg|gif)$/,
@@ -47,6 +56,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './client/index.html',
       filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
     })
   ]
 };
